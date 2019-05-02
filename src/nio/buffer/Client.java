b2b2d182fa73bdd8358892a12f2791ca8974967f
丁浩阳 <3003763007@qq.com>
2019-05-02 18:53:41 +0800

    修改

--- src/nio/buffer/Client.java
+++ src/nio/buffer/Client.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * @date 2019/4/27
  */
 public class Client {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, InterruptedException {
         //1.获取到通道
         SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8888));
         //2.切换到非阻塞模式
@@ -28,6 +28,16 @@ public class Client {
             socketChannel.write(buffer);
             buffer.clear();
             System.out.println(next);
+            int len = 0;
+            while ((len = socketChannel.read(buffer)) ==0) {
+                Thread.sleep(2000);
+                System.out.println("waiting from server");
+            }
+            buffer.flip();
+            System.out.println(new String(buffer.array(), 0, len));
+
+            // 清空缓存。
+            buffer.clear();
         }
         //5.关闭通道
         socketChannel.close();
