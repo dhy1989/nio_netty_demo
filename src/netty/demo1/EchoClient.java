a60fbdf823253a664e841debdb808a193c8b4956
dinghy <88888888>
2019-05-13 08:51:05 +0800

    netty

---
+++ src/netty/demo1/EchoClient.java
@@ -0,0 +1,44 @@
+package netty.demo1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author dinghy
+ * @date
+ */
+public class EchoClient {
+    private int port;
+    private String host;
+
+    public EchoClient(int port, String host) {
+        this.port = port;
+        this.host = host;
+    }
+
+    public void start() throws InterruptedException {
+        //线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        //客户端启动必备
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                //指明NIO进行网络通讯
+                .channel(NioSocketChannel.class)
+                //配置远程服务地址
+                .remoteAddress(new InetSocketAddress(host, port))
+                .handler(new EchoClientHandler());
+        //连接到远程节点,阻塞等待直到连接完成
+        ChannelFuture f = bootstrap.connect().sync();
+        //阻塞,直到channel关闭
+        f.channel().closeFuture().sync();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new EchoClient(9000, "127.0.0.1").start();
+    }
+}
---
+++ src/netty/demo1/EchoServerHandler.java
@@ -0,0 +1,51 @@
+package netty.demo1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author dinghy
+ * @date
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    /**
+     * 服务端读取到数据后的处理
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server Accept" + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    /**
+     * 服务端读取完数据后的处理
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 发生异常后的处理
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
