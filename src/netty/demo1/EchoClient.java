8ee6bf006128abf488e39e7925255f163d1d6038
dinghy <88888888>
2019-05-16 13:41:53 +0800

    修改

--- src/netty/demo1/EchoClient.java
+++ src/netty/demo1/EchoClient.java
@@ -26,16 +26,22 @@ public class EchoClient {
         EventLoopGroup group = new NioEventLoopGroup();
         //客户端启动必备
         Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(group)
-                //指明NIO进行网络通讯
-                .channel(NioSocketChannel.class)
-                //配置远程服务地址
-                .remoteAddress(new InetSocketAddress(host, port))
-                .handler(new EchoClientHandler());
-        //连接到远程节点,阻塞等待直到连接完成
-        ChannelFuture f = bootstrap.connect().sync();
-        //阻塞,直到channel关闭
-        f.channel().closeFuture().sync();
+        try {
+            bootstrap.group(group)
+                    //指明NIO进行网络通讯
+                    .channel(NioSocketChannel.class)
+                    //配置远程服务地址
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new EchoClientHandler());
+            //连接到远程节点,阻塞等待直到连接完成
+            ChannelFuture f = bootstrap.connect().sync();
+            //阻塞,直到channel关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
